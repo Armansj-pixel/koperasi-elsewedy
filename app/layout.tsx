6f9b657292abd5826d1e8ba9fe9e169951e26047
Arman Dion Sakti <antonius.arman.jesuits@gmail.com>
2026-06-13 14:46:32 +0700

    Add RootLayout component with metadata for PWA

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Koperasi Karyawan Elsewedy",
+  description: "PWA Koperasi PT Elsewedy Electric Indonesia",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  );
